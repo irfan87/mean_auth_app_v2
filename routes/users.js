75ca1741840ccd6c2b6db711701a6446f9c4334d
irfan87 <nerve2009@yahoo.com>
2018-02-24 21:14:29 +0800

    setup routes and cors

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+// register
+router.get('/register', (req, res, next) => {
+    res.send('Register Page');
+});
+
+// authenticate
+router.get('/authenticate', (req, res, next) => {
+    res.send('Authenticate Page');
+});
+
+// profile
+router.get('/profile', (req, res, next) => {
+    res.send('Profile Page');
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,10 +5,45 @@ const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
 
+// import config file for connection
+const config = require('./config/database');
+// import users file for routing
+const users = require('./routes/users');
+
+// connect the databae
+mongoose.connect(config.database);
+
+// check if the database connection is established or not
+mongoose.connection.on('connected', () => {
+    console.log('connected to', config.database);
+});
+
+mongoose.connection.on('error', (err) => {
+    console.log(err);
+})
+
 const app = express();
 
 const port = 3000 || process.env.PORT;
 
+// enable cors so we can use for the front-end as well
+app.use(cors());
+
+// set the client path (to use in front-end page)
+app.use(express.static(path.join(__dirname, 'client')));
+
+// body parser middleware
+app.use(bodyParser.json());
+
+// routes to user
+app.use('/users', users);
+
+// setup routes to index page
+app.get('/', (req, res) => {
+    res.send('invalid endpoint');
+})
+
+// listen to the current port
 app.listen(port, (req, res) => {
     console.log('Server started on port', port);
 })
